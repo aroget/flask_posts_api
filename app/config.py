fb238f5c20f8d9820fe4fbc9b9e5b6be3f2a27e7
Andres Roget <andres@qarrotperformance.com>
2017-05-08 07:33:09 -0400

    add create_all command

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ import os
 DEBUG = True
 
 if os.environ.get('HEROKU_ENV') is None:
+    DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/flask_demo'
 
 
--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
-from app import app
+from app import app, db
 
 port = int(os.environ.get('PORT', 5000))
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(host='0.0.0.0', port=port)
 
