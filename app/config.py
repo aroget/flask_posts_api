26fc0d77b1993609e4f1d3c615c65796f1cd98f8
Andres Roget <andres@qarrotperformance.com>
2017-05-08 07:06:20 -0400

    add Procfile

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/flask_demo'
+# SQLALCHEMY_DATABASE_URI = 'mysql://bd890f399d8ceb:bea739ec@us-cdbr-iron-east-03.cleardb.net/heroku_3fe77ae70ca22b5?reconnect=true'
 
 SECRET_KEY = 'SOPA_DE_CARACOL'
 
