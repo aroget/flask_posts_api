5440a8a3fc475141006cb2062b4408712a2f89b2
Andres Roget <andres@qarrotperformance.com>
2017-05-10 21:37:06 -0400

    add db

--- app/config.py
+++ app/config.py
@@ -2,9 +2,13 @@ import os
 
 DEBUG = True
 
+SQLALCHEMY_DATABASE_URI = 'mysql://bd890f399d8ceb:bea739ec@us-cdbr-iron-east-03.cleardb.net/heroku_3fe77ae70ca22b5?reconnect=true'
+
 if os.environ.get('HEROKU_ENV') is None:
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/flask_demo'
+
+
 
 
 SECRET_KEY = 'SOPA_DE_CARACOL'
