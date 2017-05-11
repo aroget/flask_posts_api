5535c89b838ce7c6e98edb484cb03bf8aff205b7
Andres Roget <andres@qarrotperformance.com>
2017-05-10 21:38:16 -0400

    add db

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'mysql://bd890f399d8ceb:bea739ec@us-cdbr-iron-east-03.cleardb.net/heroku_3fe77ae70ca22b5?reconnect=true'
+SQLALCHEMY_DATABASE_URI = 'mysql://bd890f399d8ceb:bea739ec@us-cdbr-iron-east-03.cleardb.net/heroku_3fe77ae70ca22b5'
 
 if os.environ.get('HEROKU_ENV') is None:
     DEBUG = False
